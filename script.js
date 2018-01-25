46f4249f02e811494293542a30ba21bb4f140d5d
ivandeschaussettes <ivan.milosevic@supinternet.fr>
2018-01-25 01:26:28 +0100

    FIXED ajax and structure

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
 window.onload = function(){
-    function loadJSON(callback) {   
-        var xobj = new XMLHttpRequest();
-            xobj.overrideMimeType("application/json");
-        xobj.open('GET', 'pokemons.json', true); // Replace 'my_data' with the path to your file
-        xobj.onreadystatechange = function () {
-              if (xobj.readyState == 4 && xobj.status == "200") {
-                callback(xobj.responseText);
-                console.log(xobj.responseText);
-              }
-        };
-        xobj.send(null);  
-     }
-     var actual_JSON = {};
-     loadJSON(function(response) {
-        // Parse JSON string into object
-          var actual_JSON = JSON.parse(response);
-          return actual_JSON;
-       }); 
-
-       console.log(Object.keys(actual_JSON));
-
-
-    var errorBlock = document.querySelector('#error-block');
-    document.forms['pokename'].onsubmit =function(){
-       var selectedPokemon = this.elements['name'].value;
-       console.log(selectedPokemon);
-       
-
-        return false;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'pokemon.json', true);
+xhr.setRequestHeader('Content-Type', 'application/json');
+xhr.onload = function() {
+    if (xhr.status === 200) {
+         var PokeInfo = JSON.parse(xhr.responseText);
+         var func = Object.keys(PokeInfo);
+        Mypokedex(PokeInfo,func);
+        }
 };
+xhr.send();
+function Mypokedex(array,keys){
+    document.forms.namedItem('pokename').onsubmit =function(){
+        return false;
+    };
+}
 };
