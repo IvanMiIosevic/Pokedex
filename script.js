0b9fcdbcf281e89bd0961a90d3fd37aa54d7a772
ivandeschaussettes <ivan.milosevic@supinternet.fr>
2018-01-29 20:59:13 +0100

    UPDATE CSS

--- script.js
+++ script.js
@@ -1,73 +1,66 @@
-window.onload = function(){
+window.onload = function () {
     var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'pokemon.json', true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.onload = function() {
-    if (xhr.status === 200) {
-         var PokeInfo = JSON.parse(xhr.responseText);
-         var func = Object.keys(PokeInfo);
-        Mypokedex(PokeInfo,func);
-        search(PokeInfo,func);
-        }
-};
-xhr.send();
-function Mypokedex(array,keys){
-    document.forms.namedItem('pokename').onsubmit =function(){
-        document.querySelector('#name').innerHTML =  "";
-        document.querySelector('#types').innerHTML =  "";
-        document.querySelector('img').src = ""; 
-        var  selectedPokemon = this.elements.namedItem('name').value;
-        for(var i = 0; i < keys.length; i++){
-        var name = nameModification(array[i].names.en);
-        if(parseInt(selectedPokemon) === array[i].national_id || selectedPokemon === name){
-            document.querySelector('#name').innerHTML += "&nbsp;" + array[i].names.en + "&nbsp;/&nbsp;" + array[i].names.fr;
-            document.querySelector('img').src = getUrl(array[i].national_id, name);
-            getTypes(array[i].types);
-            } 
-        }    
-        return false;
-    };
-}
-function nameModification(name){
-    var rawName = name;
-    var loweredName = rawName.toLowerCase();
-    var pokeclean = loweredName.replace(/\s/g, '').replace(":","").replace(".","-").replace("'","").replace("♀","f").replace("♂","m"); 
-    if(pokeclean == 'mimejr-'){
-        pokeclean = "mime-jr";
-    }
-    return pokeclean;
-}
-function getUrl(nationalId,name){
-    if(nationalId  > 721){
-        return  "http://www.pkparaiso.com/imagenes/sol-luna/sprites/animados/"+ name +".gif"; 
-       }else{
-        return  "http://www.pokestadium.com/sprites/xy/" + name +".gif";
-       }
-}
-function getTypes(types){
-    for(var j in types){
-        if( j == 0){
-        document.querySelector('#types').innerHTML += "&nbsp;" + types[j] ;
-    } else{
-        document.querySelector('#types').innerHTML += "&nbsp;/&nbsp;" + types[j] ;
-    }
-}
-}
-function search(array,keys){
-    document.querySelector('#nameinput').onkeydown = function (){
-        var current = document.querySelector('#nameinput').value;
-        var key = event.keyCode || event.charCode;
-        document.querySelector('#prediction').innerHTML ="";
-        if (current.length >= 2){
-        for(var i = 0; i < keys.length; i++){
-            var name = nameModification(array[i].names.en);
-            if(name.startsWith(current) === true){
-            document.querySelector('#prediction').innerHTML += "&nbsp;" + name;
-        }
-        }
-        } else{
-            document.querySelector('#prediction').innerHTML ="";
+    xhr.open('GET', 'pokemons.json', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function () {
+        if (xhr.status === 200) {
+            var PokeInfo = JSON.parse(xhr.responseText);
+            Mypokedex(PokeInfo);
+            search(PokeInfo);
         }
     };
+    xhr.send();
+
+    function Mypokedex(array) {
+        document.forms.namedItem('pokename').onsubmit = function () {
+            document.querySelector('#name').innerHTML = "";
+            document.querySelector('#prediction').innerHTML = "";
+            document.querySelector('#types').innerHTML = "";
+            document.querySelector('img').src = "";
+            var selectedPokemon = this.elements.namedItem('name').value;
+            for (var i in array) {
+                var name = nameModification(array[i].name);
+                if (selectedPokemon === i || selectedPokemon === name) {
+                    document.querySelector('#name').innerHTML = " Name : &nbsp;" + array[i].name;
+                    document.querySelector('img').src = getUrl(name);
+                    document.querySelector('#types').innerHTML = " Type : &nbsp;" + array[i].type;
+                    document.querySelector('#error').innerHTML = "";
+                    break;
+                } else {
+                    document.querySelector('img').src = "http://www.pokestadium.com/sprites/xy/unown-interrogation.gif";
+                    document.querySelector('#error').innerHTML = "Sorry , this Pokemon is not in our Database :(";
+                }
+            }
+            return false;
+        };
+    }
+
+    function nameModification(name) {
+        var rawName = name;
+        var loweredName = rawName.toLowerCase();
+        var pokeclean = loweredName.replace(/\s/g, '').replace(":", "").replace(".", "-").replace("'", "").replace("♀", "f").replace("♂", "m");
+        return pokeclean;
+    }
+
+    function getUrl(name) {
+        return "http://www.pokestadium.com/sprites/xy/" + name + ".gif";
+    }
+
+    function search(array, keys) {
+        document.querySelector('#nameinput').onkeydown = function () {
+            var current = document.querySelector('#nameinput').value;
+            var key = event.keyCode || event.charCode;
+            document.querySelector('#prediction').innerHTML = "";
+            if (current.length >= 2) {
+                for (var i in array) {
+                    var name = nameModification(array[i].name);
+                    if (name.startsWith(current) === true) {
+                        document.querySelector('#prediction').innerHTML += "&nbsp;" + name;
+                    }
+                }
+            } else {
+                document.querySelector('#prediction').innerHTML = "";
+            }
+        };
     }
 };
