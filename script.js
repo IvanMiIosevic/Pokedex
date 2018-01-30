3d359219c990152d493e517c0edca4e4c220a3d8
ivandeschaussettes <ivan.milosevic@supinternet.fr>
2018-01-30 01:25:48 +0100

    UPDATE CSS // minor fixes

--- script.js
+++ script.js
@@ -16,15 +16,22 @@ window.onload = function () {
             document.querySelector('#name').innerHTML = "";
             document.querySelector('#prediction').innerHTML = "";
             document.querySelector('#types').innerHTML = "";
-            document.querySelector('img').src = "";
+            document.querySelector('img').src = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+            document.querySelector('#moves').innerHTML = "";
             var selectedPokemon = this.elements.namedItem('name').value;
             for (var i in array) {
                 var name = nameModification(array[i].name);
                 if (selectedPokemon === i || selectedPokemon === name) {
-                    document.querySelector('#name').innerHTML = " Name : &nbsp;" + array[i].name;
+                    document.querySelector('#name').innerHTML = " <span class='title'>Name :</span> &nbsp;" + array[i].name;
                     document.querySelector('img').src = getUrl(name);
-                    document.querySelector('#types').innerHTML = " Type : &nbsp;" + array[i].type;
+                    document.querySelector('#types').innerHTML = " <span class='title'>Type :</span> &nbsp;" + array[i].type;
+                    document.querySelector('#stats').innerHTML="Stats : ";
+                    document.querySelector('#attack').innerHTML="Attack : " + array[i].attack;
+                    document.querySelector('#defense').innerHTML="Defense : " + array[i].defense;
+                    document.querySelector('#movesInfo').innerHTML="Moves : ";
+                    document.querySelector('#moves').innerHTML = getAttacks(array,i);
                     document.querySelector('#error').innerHTML = "";
+                    getAttacks(array,i);
                     break;
                 } else {
                     if(parseInt(selectedPokemon)){
@@ -44,14 +51,26 @@ window.onload = function () {
     function nameModification(name) {
         var rawName = name;
         var loweredName = rawName.toLowerCase();
-        var pokeclean = loweredName.replace(/\s/g, '').replace(":", "").replace(".", "-").replace("'", "").replace("♀", "f").replace("♂", "m");
+        var firstTweak = loweredName.replace(/\s/g, '').replace(":", "").replace(".", "-");
+        var nidoranTweak = firstTweak.replace("♀", "f").replace("♂", "m");
+        var pokeclean = nidoranTweak.replace("'", "");
         return pokeclean;
     }
 
     function getUrl(name) {
         return "http://www.pokestadium.com/sprites/xy/" + name + ".gif";
     }
-
+    function getAttacks(array, number) {
+        var moves = "";
+        for (var i in array[number].moves){
+            if(i != 1){
+            moves += "&nbsp" + array[number].moves[i] + "&nbsp/";
+        }else{
+            moves += "&nbsp" + array[number].moves[i] + "&nbsp<br>";
+        }
+    }
+        return moves;
+    }
     function search(array, keys) {
         document.querySelector('#nameinput').onkeydown = function () {
             var current = document.querySelector('#nameinput').value;
